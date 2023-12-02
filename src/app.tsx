91d2dc9fdee6bc0cbe4427b3ef5f137d7f9184d4
Alexander <aaalex2025@gmail.com>
2023-12-01 22:03:58 -0400

    project init it works for other projects

---
+++ src/app.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { Text, View } from 'react-native'
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <View>
+        <Text>Hola mundo</Text>
+      </View>
+    </NavigationContainer>
+  )
+}
+export default App
